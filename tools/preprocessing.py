d1b7f040d9c9ee336b4914a6a139f2daf51e72c0
yihaoding <81132254+yihaoding@users.noreply.github.com>
2021-04-20 10:51:25 +1000

    Update preprocessing.py

--- tools/preprocessing.py
+++ tools/preprocessing.py
@@ -34,7 +34,9 @@ def pyMuPDF_fitz(pdfPath, imagePath,count):
         count +=1
     return count
 
-def coco_test_json_generator(img_path,output_path):
+def coco_test_json_generator(img_path,output_path,test_json_path):
+  with open(test_json_path) as f:
+    test_json = json_load(f)
   img_name_list = os.listdir(img_path)
   new_json = {}
   new_json['images'] = []
